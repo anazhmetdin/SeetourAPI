30da30ea393c1603efa294463798efea8b3000ec
Mahmoud Sherif <127897127+MahmoudSherif74@users.noreply.github.com>
2023-04-17 01:53:45 +0200

    AdminRepo?updates

--- SeetourAPI/DAL/Repos/IAdminRepo.cs
+++ SeetourAPI/DAL/Repos/IAdminRepo.cs
@@ -7,12 +7,12 @@ namespace SeetourAPI.DAL.Repos
     {
         //for Seetour user cause it has all the properties
         public IEnumerable<SeetourUser> GetAll();
+        public IEnumerable<SeetourUser> GetUsers();
+        public IEnumerable<SeetourUser> GetTourGuides();
         public SeetourUser GetSeeTourUserById(string id);
-        public SeetourUser? EditSeeTourUser(string id, SeetourUser seetourUser );
-        public void AddSeeTourUser(SeetourUser seeTourUser);
+        public void EditTourGuide(string id, SeetourUser seetourUser );
+        public void EditCustomer(string id, SeetourUser seetourUser);
         public void DeleteSeeTourUser(string id);
-
-        //also to update role  must have 
         public void updateRole(string id,string securitylevel);
 
     }
