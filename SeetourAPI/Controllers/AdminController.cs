06557365026659baccb36277a7212772d58683bd
Mahmoud Sherif <127897127+MahmoudSherif74@users.noreply.github.com>
2023-04-18 01:04:23 +0200

    admin Controller added

--- SeetourAPI/Controllers/AdminController.cs
+++ SeetourAPI/Controllers/AdminController.cs
@@ -1,13 +1,84 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SeetourAPI.BL.AdminManger;
+using SeetourAPI.Data.Models.Users;
 
 namespace SeetourAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    
+
     public class AdminController : ControllerBase
     {
+        private readonly IAdminManger _adminManager;
+
+        public AdminController(IAdminManger adminManager)
+        {
+            _adminManager = adminManager;
+        }
+
+        [HttpGet("all")]
+        public ActionResult<IEnumerable<SeetourUser>> GetAllUsers()
+        {
+            var users = _adminManager.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<SeetourUser> GetUserById(string id)
+        {
+            var user = _adminManager.GetSeeTourUserById(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpGet("users")]
+        public ActionResult<IEnumerable<SeetourUser>> GetUsers()
+        {
+            var users = _adminManager.GetUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("guides")]
+        public ActionResult<IEnumerable<SeetourUser>> GetTourGuides()
+        {
+            var guides = _adminManager.GetTourGuides();
+            return Ok(guides);
+        }
+
+        [HttpPut("{id}/role")]
+        public ActionResult UpdateRole(string id, [FromBody] string securitylevel)
+        {
+            _adminManager.updateRole(id, securitylevel);
+            return NoContent();
+        }
+
+        [HttpPut("{id}/customer")]
+        public ActionResult EditCustomer(string id, [FromBody] SeetourUser seetourUser)
+        {
+            _adminManager.EditCustomer(id, seetourUser);
+            return NoContent();
+        }
+
+        [HttpPut("{id}/tourguide")]
+        public ActionResult EditTourGuide(string id, [FromBody] SeetourUser seetourUser)
+        {
+            _adminManager.EditTourGuide(id, seetourUser);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteSeeTourUser(string id)
+        {
+            _adminManager.DeleteSeeTourUser(id);
+            return NoContent();
+        }
     }
+
 }
--- SeetourAPI/Data/Models/Review.cs
+++ SeetourAPI/Data/Models/Review.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using SeetourAPI.Data.Models.Photos;
 
 namespace SeetourAPI.Data.Models
 {
@@ -11,7 +12,7 @@ namespace SeetourAPI.Data.Models
         public int Rating { get; set; }
         [StringLength(512, MinimumLength = 32)]
         public string Comment { get; set; } = string.Empty;
-        public virtual ICollection<Photo> Photos { get; set; } = new HashSet<Photo>();
+        public virtual ICollection<ReviewPhoto> Photos { get; set; } = new HashSet<ReviewPhoto>();
         public DateTime LastEditedAt { get; set; } = DateTime.UtcNow;
     }
 }
